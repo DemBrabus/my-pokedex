af716ab02451cb2eeca05bc623ccd80f61904f90
Dan Montoya <dan@huemordesigns.com>
2021-04-29 08:39:48 -0400

    first update, setting up Styled Components, set up some simple fetch examples, changed index page.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
+
 import '../styles/globals.css'
 
+// Styled Components
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: #fffefe;
+  }
+`;
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <GlobalStyle />
+      <ThemeProvider theme={theme} >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </div>
+  )
 }
 
 export default MyApp
